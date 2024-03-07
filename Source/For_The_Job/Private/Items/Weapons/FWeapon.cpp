87381d0c02ebeb822d4ffc3bbe99f1f5d465d861
river-zero <lero_ro@naver.com>
2024-03-07 19:07:50 +0900

    추가: 무기 충돌 감지

--- Source/For_The_Job/Private/Items/Weapons/FWeapon.cpp
+++ Source/For_The_Job/Private/Items/Weapons/FWeapon.cpp
@@ -3,6 +3,22 @@
 #include "Kismet/GameplayStatics.h"
 #include "Sound/SoundBase.h"
 #include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+
+AFWeapon::AFWeapon() {
+	WeaponBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon Box"));
+	WeaponBox->SetupAttachment(GetRootComponent());
+
+	WeaponBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	WeaponBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	WeaponBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	BoxTraceStart = CreateAbstractDefaultSubobject<USceneComponent>(TEXT("Box Trace Start"));
+	BoxTraceStart->SetupAttachment(GetRootComponent());
+
+	BoxTraceEnd = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace End"));
+	BoxTraceEnd->SetupAttachment(GetRootComponent());
+}
 
 void AFWeapon::Equip(USceneComponent *InParent, FName InSocketName) {
 	AttachMeshToSocket(InParent, InSocketName);
@@ -27,10 +43,39 @@ void AFWeapon::AttachMeshToSocket(USceneComponent *InParent, const FName &InSock
 	ItemMesh->AttachToComponent(InParent, TransformRules, InSocketName);
 }
 
+void AFWeapon::BeginPlay() {
+	Super::BeginPlay();
+
+	WeaponBox->OnComponentBeginOverlap.AddDynamic(this, &AFWeapon::OnBoxOverlap);
+}
+
 void AFWeapon::OnSphereOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
 }
 
 void AFWeapon::OnSphereEndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex) {
 	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
+
+void AFWeapon::OnBoxOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) {
+	// 무기에 부착된 박스의 충돌 감지
+	const FVector Start = BoxTraceStart->GetComponentLocation();
+	const FVector End = BoxTraceEnd->GetComponentLocation();
+
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+	FHitResult BoxHit;
+	UKismetSystemLibrary::BoxTraceSingle(
+		this,
+		Start,
+		End,
+		FVector(5.f, 5.f, 5.f),
+		BoxTraceStart->GetComponentRotation(),
+		ETraceTypeQuery::TraceTypeQuery1,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		BoxHit,
+		true
+	);
 }
--- Source/For_The_Job/Public/Items/Weapons/FWeapon.h
+++ Source/For_The_Job/Public/Items/Weapons/FWeapon.h
@@ -5,22 +5,39 @@
 #include "FWeapon.generated.h"
 
 class USoundBase;
+class UBoxComponent;
 
 UCLASS()
 class FOR_THE_JOB_API AFWeapon : public AFItem {
 	GENERATED_BODY()
 
 public:
+	AFWeapon();
+
 	void Equip(USceneComponent *InParent, FName InSocketName);
 
 	void AttachMeshToSocket(USceneComponent *InParent, const FName &InSocketName);
 	
 protected:
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult) override;
 
 	virtual void OnSphereEndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex) override;
 
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
 private:
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
 	USoundBase *EquipSound;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	UBoxComponent *WeaponBox;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent *BoxTraceStart;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent *BoxTraceEnd;
 };
